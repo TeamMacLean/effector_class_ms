1957189d1147fe4235692d329871a3fbece37dc2
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-06-07 17:31:09 +0100

    Updated scripts used for processing confusion matrices vs other tools

--- data/processed/oomycete_conf_mat_processing.R
+++ data/processed/oomycete_conf_mat_processing.R
@@ -1,6 +1,31 @@
 library(tidyverse)
 
-all_oomycete_pred <-  data.table::fread("data/raw/oomycete_pred_all.csv", drop = c("V1", "sequence"))
+
+# Read all results from effectR
+pred_result_fungi_effectR <- data.table::fread("data/raw/oomycete_pred_effectR_testing.csv", drop = c("V1")) %>% 
+  dplyr::rename(effectR = pred)
+
+# Get the prediction results from deepredeff
+oomycete_pred_ens <-  data.table::fread("data/raw/oomycete_pred_ens.csv", drop = c("V1"))
+
+# Get the real data 
+oomycete_test_data <- data.table::fread("data/raw/oomycete_testing.csv")
+
+# Combine real data and deepredeff results to get the true label
+
+oomycete_pred_ens <- oomycete_pred_ens %>% 
+  dplyr::left_join(., oomycete_test_data, by = c("sequence" = "Sequence")) 
+
+
+all_oomycete_pred <- oomycete_pred_ens %>% 
+  dplyr::left_join(., pred_result_fungi_effectR, by = c("sequence" = "id", "label" = "true_label")) %>% 
+  # if they are not predicted as effector, then non-effector
+  dplyr::mutate(effectR = ifelse(is.na(effectR), 0, effectR)) %>% 
+  dplyr::rename(true_label = label)
+
+# Save to the model
+readr::write_csv(all_oomycete_pred, "data/processed/all_oomycete_pred_deepredeff_effectR.csv")
+
 
 model_list <- c("cnn_lstm", "effectR")
 
